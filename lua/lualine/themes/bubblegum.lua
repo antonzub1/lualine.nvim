c6925474cc072431ee644b5519893100741f7479
Anton Zub <azb.0x7dc@gmail.com>
2025-04-05 14:09:14 +0300

    feat: Update colors

--- lua/lualine/themes/bubblegum.lua
+++ lua/lualine/themes/bubblegum.lua
@@ -1,31 +1,29 @@
 local colors = {
   gray = "#303030",
-  light_gray = "#444444",
   green = "#aed786",
   blue = "#87afd7",
   pink = "#d7afd7",
   red = "#d78786",
 }
-
 return {
   normal = {
-    a = { fg = colors.green, bg = colors.gray, gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.green },
+    a = { fg = colors.gray, bg = colors.green, gui = 'bold' },
+    b = { fg = colors.green, bg = colors.gray },
+    c = { fg = colors.green, bg = colors.gray },
   },
   insert = {
-    a = { fg = colors.blue, bg = colors.gray, gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.blue },
+    a = { fg = colors.gray, bg = colors.blue, gui = 'bold' },
+    b = { fg = colors.blue, bg = colors.gray },
+    c = { fg = colors.blue, bg = colors.gray },
   },
   visual = {
-    a = { fg = colors.pink, bg = colors.gray,  gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.pink },
-  },
-  inactive = {
-    a = { fg = colors.light_gray, bg = colors.gray, gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.light_gray },
+    a = { fg = colors.gray, bg = colors.pink,  gui = 'bold' },
+    b = { fg = colors.pink, bg = colors.gray },
+    c = { fg = colors.pink, bg = colors.gray },
   },
   replace = {
-    a = { fg = colors.red, bg = colors.gray, gui = 'bold' },
-    b = { fg = colors.gray, bg = colors.red },
+    a = { fg = colors.gray, bg = colors.red, gui = 'bold' },
+    b = { fg = colors.red, bg = colors.gray },
+    c = { fg = colors.red, bg = colors.gray },
   },
 }
